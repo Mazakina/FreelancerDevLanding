8fd91832a4ac13367373f2aefe72071b647d962a
Paulo Mazakina <8.mazakina@gmail.com>
2023-08-12 00:21:30 -0300

    develop of functional parts of project display

--- src/components/SkillsSec/display.tsx
+++ src/components/SkillsSec/display.tsx
@@ -2,6 +2,17 @@ import FirstImage from '@images/htmlImgWebp.webp'
 import Project2 from '@images/Rect1.png'
 import Project3 from '@images/projeto2 copy.png'
 import Image from 'next/image'
+import { Anton, Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight:"700",
+  subsets:['latin']
+})
+
+const anton = Anton({
+  weight:"400",
+  subsets:['latin']
+})
 
 export default function Display(){
   const allProjects = [
@@ -48,21 +59,22 @@ export default function Display(){
 
   return(
     <div className="box-border border-2 rounded-sm border-c-gray100 display w-3/5 mt-20 mx-auto h-120 relative overflow-hidden">
-      <div className=" box-content border-l-2 rounded-sm border-c-gray100 overflow-hidden w-4/5 bg-gray-600 absolute h-full right-0">
-        <Image height={'480'} width='1440' alt=""  style={{ objectPosition:"60px -60px", filter:"blur(2px)", height:'200%', objectFit:'cover', }} src={FirstImage.src} />
+      <div className="cursor-pointer box-content border-l-2 rounded-sm border-c-gray100 overflow-hidden -skew-x-25 w-4/5 bg-gray-600 absolute h-full right-0">
+        <Image height={'480'} width='1440' alt=""  style={{transform:"skew(25deg,0) translatex(-150px)", objectPosition:"60px -60px", filter:"blur(2px)", height:'200%', objectFit:'cover', }} src={FirstImage.src} />
       </div>
-      <div className=" box-content border-l-2 rounded-sm border-c-gray100
+      <div className="cursor-pointer box-content border-l-2 rounded-sm border-c-gray100
       overflow-hidden -skew-x-25 w-3/6 absolute h-full right-4">
         <Image height={'480'} width='1440' alt=""  style={{transform:"skew(25deg,0) translatex(-130px)",height:'100%',width:'1000px',maxWidth:'1000px',position:'absolute' ,objectPosition:"-20px 0" ,filter:"blur(1px)", objectFit:'cover', }} src={Project2.src} />
       </div>
-      <div className=" box-content border-l-2 rounded-sm border-c-gray100 overflow-hidden -skew-x-25 w-1/3 absolute h-full -right-32">
+      <div className="cursor-pointer box-content border-l-2 rounded-sm border-c-gray100 overflow-hidden -skew-x-25 w-1/3 absolute h-full -right-32">
         <Image height={'480'} width='1440' alt=""  style={{transform:"skew(25deg,0) translatex(-80px)",height:'140%',width:'800px',maxWidth:'800px',position:'absolute',objectPosition:"-70px -150px",filter:"blur(1px)", objectFit:'cover', }} src={Project3.src} />
       </div>
-      <div className="z-30 bg-white absolute h-full -right-32">
-        <Image height={'480'} width='1440' alt=""  style={{transform:"skew(25deg,0) translatex(-80px)",height:'100%',width:'100%',position:'absolute', objectFit:'cover', }} src={''} />
-      </div>
-      <div>
-         
+      <div className=" z-30 bg-white absolute h-full w-full ">
+        <Image height={'480'} width='1440' alt=""  style={{height:'100%',width:'100%',position:'absolute', objectFit:'cover', }} src={Project3.src} />
+        <div className="flex flex-col absolute right-0 h-full w-1/3 bg-transparent backdrop-blur-sm border-l-2 backdrop-brightness-50">
+          <h2 className={`text-white text-xl ${anton.className} mx-auto mt-6 `} >Titulo</h2>
+          <p className={`text-white text-xl ${anton.className} mx-auto mt-6 `}>Aplicativo JAM-Stack, feito para que artistas brasileiros divulguem suas artes e possiveis clientes entrem em contato. Utilizando de um layout interativo onde se pode arrastar imagens para dentro de albums ou serem deletadas</p>
+        </div>
       </div>
     </div>
   )
