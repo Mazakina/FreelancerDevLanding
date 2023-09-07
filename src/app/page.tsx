af0a32c6614dcd5c2bc21e5568aa631cd8f5f7f9
Paulo Mazakina <8.mazakina@gmail.com>
2023-09-06 23:08:08 -0300

    initial: about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import localFont from 'next/font/local'
 import { Anton, Roboto } from "next/font/google"
 import HomeSec from '@/components/HomeSec'
 import SkillsSec from '@/components/SkillsSec'
+import AboutMe from '@/components/AboutMe'
 const blanka = localFont({ src: '../../public/Blanka.otf' })
 
 const anton = Anton({
@@ -24,6 +25,7 @@ export default async function Home() {
         <Header className="p-auto ml-24 mr-4 z-10"/>
         <HomeSec/>
         <SkillsSec/>
+        <AboutMe/>
       </section>
 
     </main>
---
+++ src/components/AboutMe/index.tsx
@@ -0,0 +1,18 @@
+export default function AboutMe(){
+  return(
+    <div className="flex-col text-cyan-50 bg-c-gray100 ">
+      <h3 className="mx-auto">SOBRE MIM</h3>
+      <div className="grid grid-cols-2 grid-rows-2 text-cyan-50">
+        <div className='row-span-2 col-span-1'>
+         IMAGE
+        </div>
+        <div className='row-span-1 col-span-1'>
+        1
+        </div>
+        <div className='row-span-1 col-span-1'>
+        2
+        </div>
+      </div>
+    </div>
+  )
+}
